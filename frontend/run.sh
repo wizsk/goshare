846a07a18e723e841ae606304767866b8e5dd964
sakib <sakibul706@gmail.com>
2023-12-20 11:45:37 +0600

    working on the mobile phone style mobile phone

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,1 +1,1 @@
-npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
+npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
--- frontend/src/zip.js
+++ frontend/src/zip.js
@@ -1,57 +1,68 @@
+// main.js
 
-// zip.js
-
-const zipDownBtn = document.getElementById("zip-download");
-let isZippin = false;
-
-zipDownBtn.addEventListener("click", () => {
-    if (isZippin) return;
-    zipDownBtn.disabled = true;
-    isZippin = true;
-
-    const url = [];
+window.onload = () => {
+    // runs every time to clear the selections
     items.forEach((itm) => {
-        if (itm.checked) {
-            url.push(itm.getAttribute(dataZipSelect));
-        }
+        itm.checked = false;
     });
+    filesToZipCount = 0;
+    showNotShowZipDown();
+}
 
-    const strr = url.map(itm => `files=${encodeURIComponent(itm)}`).join("&");
-    const path = `/zip?${strr}&cwd=${encodeURIComponent(window.location.pathname)}`; //&files=${encodeURIComponent("/../../../")}`;
+const dataZipSelect = "data-zip-select";
+const items = document.querySelectorAll(`[${dataZipSelect}]`);
+const dataZipDown = document.getElementById("zip-download");
+// const zipSelectAllBtn = document.getElementById("selectAll-button");
+const zipClearSelectionBtn = document.getElementById("clearSelections-button");
+let filesToZipCount = 0;
 
-    console.log("SSE reqest to:", path);
-    const sse = new EventSource(path);
-
-    sse.onopen = () => {
-        zipDownBtn.disabled = true;
-        isZippin = true;
-        console.log("sse opended");
+/** shows or hides downzip button, zipClearSelections buttton */
+function showNotShowZipDown() {
+    if (filesToZipCount > 0) {
+        dataZipDown.style.display = "block";
+        zipClearSelectionBtn.style.display = "block";
+        console.log("zip down showing:", filesToZipCount);
+    } else {
+        dataZipDown.style.display = "none";
+        zipClearSelectionBtn.style.display = "none";
+        console.log("zip down not showing:", filesToZipCount);
     }
+}
 
-    sse.onerror = (err) => {
-        zipDownBtn.disabled = false;
-        isZippin = false;
-        console.error(err);
-        sse.close();
+function showHideZipOptions() {
+    document.getElementById("zip-options").classList.toggle("hidden");
+}
+
+
+/** selectAll function selets all the file in current direcotr for zipping */
+function selectAll() {
+    items.forEach((itm) => {
+        itm.checked = true;
+    });
+    filesToZipCount = items.length;
+    showNotShowZipDown();
+}
+
+
+/** clearSelections function deselets all the file in current direcotr for zipping */
+function clearSelections() {
+    items.forEach((itm) => {
+        itm.checked = false;
+    });
+
+    filesToZipCount = 0;
+    showNotShowZipDown();
+}
+
+
+/**
+ * @param {HTMLInputElement} itm - htmlInputElelemt type checkBox
+ */
+function markUnmark(itm) {
+    if (itm.checked) {
+        filesToZipCount++;
+    } else {
+        filesToZipCount--;
     }
-
-    // done event
-    sse.addEventListener("done", async (e) => {
-        zipDownBtn.disabled = false;
-        isZippin = false;
-        console.log("sse done:", e.data);
-        sse.close();
-    });
-
-    sse.addEventListener("onProgress", async (e) => {
-        console.log("sse onPgoress", e.data);
-    });
-
-    sse.onclose = () => {
-        zipDownBtn.disabled = false;
-        isZippin = false;
-        console.log("SSE connection closed.");
-    };
-
-    // fetch(fo);
-});
+    showNotShowZipDown();
+}
